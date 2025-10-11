a1f1e3826022392b9fed589e8923364265f12326
Mehanth <mehanth2310579@ssn.edu.in>
2025-10-12 05:03:06 +0530

    route issue

--- bigdata-dashboard-bu/src/app/app.routes.ts
+++ bigdata-dashboard-bu/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Redirect empty path to '/dashboard'
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  
+  // Define the dashboard route
+  { path: 'dashboard', component: DashboardComponent }
+];
